cfa25746f5626fc02060d3f545441621376cf9e7
George Javaras <fr33k3r@yahoo.com>
2017-07-31 14:50:22 +0300

    Add Views

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MvcMusicStore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "A Story about a Music Store";
 
             return View();
         }
