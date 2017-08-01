f4ae2f0a973b86721dd8bb2f0b7454be2e4cefc4
George Javaras <fr33k3r@yahoo.com>
2017-08-01 12:27:23 +0300

    End of Chapter 6

---
+++ MvcMusicStore/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using MvcMusicStore.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(160)]
+        [Display(Name = "Last Name", Order = 15001)]
+        [MaxWords(10, ErrorMessage = "There are too many words in {0}")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(160, MinimumLength = 3)]
+        [Display(Name = "First Name", Order = 15000)]
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+                ErrorMessage = "Email doesn't look like a valid email address.")]
+        public string Email { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:c}")]
+        public decimal Total { get; set; }
+        //public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ MvcMusicStore/Models/OrderDetail.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class OrderDetail
+    {
+    }
+}
